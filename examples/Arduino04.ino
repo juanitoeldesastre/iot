1fdf4ab8c8e8843336165d60338a8fc3cc54fe52
juanitoeldesastre <juanitoeldevastador@gmail.com>
2025-04-17 18:17:29 -0500

    Agregar primeros archivos de arduino como ejemplo

---
+++ examples/Arduino04.ino
@@ -0,0 +1,112 @@
+int pin1 = 2;
+int pin2 = 3;
+int pin3 = 4;
+int pin4 = 5;
+int delaytime = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(pin1,OUTPUT);
+pinMode(pin2,OUTPUT);
+pinMode(pin3,OUTPUT);
+pinMode(pin4,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(pin1, LOW);
+digitalWrite(pin2, LOW);
+digitalWrite(pin3, LOW);
+digitalWrite(pin4, LOW);
+delay(delaytime);
+
+digitalWrite(pin1, LOW);
+digitalWrite(pin2, LOW);
+digitalWrite(pin3, LOW);
+digitalWrite(pin4, HIGH);
+delay(delaytime);
+
+digitalWrite(pin1, LOW);
+digitalWrite(pin2, LOW);
+digitalWrite(pin3, HIGH);
+digitalWrite(pin4, LOW);
+delay(delaytime);
+
+digitalWrite(pin1, LOW);
+digitalWrite(pin2, LOW);
+digitalWrite(pin3, HIGH);
+digitalWrite(pin4, HIGH);
+delay(delaytime);
+
+digitalWrite(pin1, LOW);
+digitalWrite(pin2, HIGH);
+digitalWrite(pin3, LOW);
+digitalWrite(pin4, LOW);
+delay(delaytime);
+
+digitalWrite(pin1, LOW);
+digitalWrite(pin2, HIGH);
+digitalWrite(pin3, LOW);
+digitalWrite(pin4, HIGH);
+delay(delaytime);
+
+digitalWrite(pin1, LOW);
+digitalWrite(pin2, HIGH);
+digitalWrite(pin3, HIGH);
+digitalWrite(pin4, LOW);
+delay(delaytime);
+
+digitalWrite(pin1, LOW);
+digitalWrite(pin2, HIGH);
+digitalWrite(pin3, HIGH);
+digitalWrite(pin4, HIGH);
+delay(delaytime);
+
+digitalWrite(pin1, HIGH);
+digitalWrite(pin2, LOW);
+digitalWrite(pin3, LOW);
+digitalWrite(pin4, LOW);
+delay(delaytime);
+
+digitalWrite(pin1, HIGH);
+digitalWrite(pin2, LOW);
+digitalWrite(pin3, LOW);
+digitalWrite(pin4, HIGH);
+delay(delaytime);
+
+digitalWrite(pin1, HIGH);
+digitalWrite(pin2, LOW);
+digitalWrite(pin3, HIGH);
+digitalWrite(pin4, LOW);
+delay(delaytime);
+
+digitalWrite(pin1, HIGH);
+digitalWrite(pin2, LOW);
+digitalWrite(pin3, HIGH);
+digitalWrite(pin4, HIGH);
+delay(delaytime);
+
+digitalWrite(pin1, HIGH);
+digitalWrite(pin2, HIGH);
+digitalWrite(pin3, LOW);
+digitalWrite(pin4, LOW);
+delay(delaytime);
+
+digitalWrite(pin1, HIGH);
+digitalWrite(pin2, HIGH);
+digitalWrite(pin3, LOW);
+digitalWrite(pin4, HIGH);
+delay(delaytime);
+
+digitalWrite(pin1, HIGH);
+digitalWrite(pin2, HIGH);
+digitalWrite(pin3, HIGH);
+digitalWrite(pin4, LOW);
+delay(delaytime);
+
+digitalWrite(pin1, HIGH);
+digitalWrite(pin2, HIGH);
+digitalWrite(pin3, HIGH);
+digitalWrite(pin4, HIGH);
+delay(delaytime);
+}
